f152fec6088299ac2f0b9109e7e52505dd3f7dc4
Adrian Nadeau <adriannadeau.art@gmail.com>
2023-10-31 15:55:33 -0300

    Update CeilingFanTest.java
    Removed Empty lines

--- src/CeilingFanTest.java
+++ src/CeilingFanTest.java
@@ -1,36 +1,27 @@
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
-
  /**
  * The class Ceiling fan test
  */ 
 public class CeilingFanTest {
-
     private CeilingFan fan;
-
     @Before
-
 /** 
  *
  * Sets the up
  *
  */
     public void setUp() { 
-
         fan = new CeilingFan();
     }
-
     @Test
-
 /** 
  *
  * Test pull cord1
  *
  */
     public void testPullCord1() { 
-
         fan.pullFanCord1();
         assertEquals(1, fan.getTotalSpeed());
         fan.pullFanCord1();
@@ -41,26 +32,19 @@ public class CeilingFanTest {
         //test failed result
         fan.pullFanCord1();
         assertNotEquals(4, fan.getTotalSpeed());
-       
     }
-
     @Test
-
 /** 
  *
  * Test pull cord2
  *
  */
     public void testPullCord2() { 
-
         fan.pullFanCord2();
         assertTrue(fan.isReversed());
-    
        fan.pullFanCord2();
         assertTrue(!fan.isReversed());
-    
         fan.pullFanCord2();
         assertTrue(fan.isReversed());
     }
-    
 }
